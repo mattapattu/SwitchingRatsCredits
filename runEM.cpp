0777b82a423db2d6c617f59a41823116767f2f16
mattapattu <mattapattu@gmail.com>
2024-01-13 13:25:14 +0100

    latest

--- runEM.cpp
+++ runEM.cpp
@@ -16,10 +16,10 @@ int main(int argc, char* argv[])
     // std::string s4ObjectName = "ratdata";
     RInside R;
 
-    std::vector<std::string> rats = {"rat106","rat112"};
+    // std::vector<std::string> rats = {"rat106","rat112"};
 
-    // std::string rat = argv[1];
-    // std::vector<std::string> rats = {rat};
+    std::string rat = argv[1];
+    std::vector<std::string> rats = {rat};
 
     for(const std::string& ratName: rats)
     {
