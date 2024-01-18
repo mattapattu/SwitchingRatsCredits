d47927182b4e8c64b757ccf43ab57b13a28fed94
mattapattu <mattapattu@gmail.com>
2024-01-18 10:47:43 +0100

    latest

--- Simulation.cpp
+++ Simulation.cpp
@@ -287,7 +287,7 @@ RatData generateSimulation(RatData& ratdata, MazeGraph& suboptimalHybrid3, MazeG
     int sessions = uniqSessIdx.n_elem;
 
 
-    RecordResults allRes =  runEM(rdata, suboptimalHybrid3, optimalHybrid3, params,clusterParams, true);
+    RecordResults allRes =  runEM(ratdata, suboptimalHybrid3, optimalHybrid3, params,clusterParams, false);
     std::map<std::string, std::vector<double>>& trueRewardVecMapS0 = allRes.getRewardVectorS0();
     std::map<std::string, std::vector<double>>& trueRewardVecMapS1 = allRes.getRewardVectorS1();
     // std::random_device rd;
@@ -500,20 +500,20 @@ RatData generateSimulation(RatData& ratdata, MazeGraph& suboptimalHybrid3, MazeG
                 //     s0rewardsSubOpt = {0,0,0,0,0,0,reward,0,0,0,0,0};
                 // }
 
-                std::vector<double> trueRewards_pair.firstS0 = trueRewardVecMapS0[trueStrategy1->getName()];
+                std::vector<double> trueRewards_pair_firstS0 = trueRewardVecMapS0[trueStrategy1->getName()];
 
-                std::vector<double> trueRewards_pair.secondS0 = trueRewardVecMapS0[trueStrategy2->getName()];
-                std::vector<double> trueRewards_pair.secondS1 = trueRewardVecMapS1[trueStrategy2->getName()];    
+                std::vector<double> trueRewards_pair_secondS0 = trueRewardVecMapS0[trueStrategy2->getName()];
+                std::vector<double> trueRewards_pair_secondS1 = trueRewardVecMapS1[trueStrategy2->getName()];    
 
                 
-                trueStrategy1->setRewardsS0(trueRewards_pair.firstS0[ses]);
+                trueStrategy1->setRewardsS0(trueRewards_pair_firstS0[ses]);
 
-                trueStrategy2->setRewardsS0(trueRewards_pair.secondS0[ses]);
-                trueStrategy2->setRewardsS1(trueRewards_pair.secondS1[ses]);
+                trueStrategy2->setRewardsS0(trueRewards_pair_secondS0[ses]);
+                trueStrategy2->setRewardsS1(trueRewards_pair_secondS1[ses]);
 
-                randomPair.first->setRewardsS0(trueRewards_pair.firstS0[ses]);
-                randomPair.second->setRewardsS0(trueRewards_pair.secondS0[ses]);
-                randomPair.second->setRewardsS1(trueRewards_pair.secondS1[ses]);
+                randomPair.first->setRewardsS0(trueRewards_pair_firstS0[ses]);
+                randomPair.second->setRewardsS0(trueRewards_pair_secondS0[ses]);
+                randomPair.second->setRewardsS1(trueRewards_pair_secondS1[ses]);
 
 
                 //Start suboptimal portion of switching simulations
