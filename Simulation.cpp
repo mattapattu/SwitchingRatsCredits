bda9df4c8b177040281ebf18c7874fa78f2b9a97
mattapattu <mattapattu@gmail.com>
2024-12-03 16:43:31 +0100

    latest

--- Simulation.cpp
+++ Simulation.cpp
@@ -1120,10 +1120,10 @@ void testSims(RatData& ratdata, MazeGraph& suboptimalHybrid3, MazeGraph& optimal
         simClusterParams = {0.07, 0.37, 0.93, 0.85, 0.14, 0.12, 0.03, 0.90, 1.88};
     }else if(ratdata.getRat() == "rat_106")
     {
-        simClusterParams = {0.14, 1.00, 0.26, 0.92, 0.73, 0.88, 0.07, 0.43, 0.30};
+        simClusterParams = {0.32, 0.56, 0.26, 0.92, 0.73, 0.88, 0.07, 0.43, 4.03};
     }else if(ratdata.getRat() == "rat_112")
     {
-        simClusterParams = {0.25, 0.00, 0.84, 1.00, 0.99, 1.00, 0.02, 0.65, 0.00};
+        simClusterParams = {0.077, 0.19, 0.71, 0.85, 0.66, 0.31, 0.02, 0.65, 4.18};
     }else if(ratdata.getRat() == "rat_113")
     {
         simClusterParams = {0.26, 0.46, 0.94, 0.80, 0.14, 0.81, 0.05, 0.72, 1.63};
